048a95de5c4b6ff378d590a3e43d7702702e126b
Broken Outlaws <145587592+BrokenOutlaws@users.noreply.github.com>
2025-10-20 14:53:56 -0400

    Delete broken_outlaws_stool directory

--- broken_outlaws_stool/EXTRA/item_db.sql
+++
@@ -1,7 +0,0 @@
--- stool/chair items
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('old_foldable_stool', 'Old Foldable Stool', 5, 1, 'item_standard', 1);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('new_foldable_stool', 'New Foldable Stool', 5, 1, 'item_standard', 1);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('old_foldable_chair', 'Old Foldable Chair', 5, 1, 'item_standard', 1);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('new_foldable_chair', 'New Foldable Chair', 5, 1, 'item_standard', 1);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('luxury_foldable_chair', 'Luxury Foldable Chair', 5, 1, 'item_standard', 1);
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('foldable_beach_chair', 'Foldable Beach Chair', 150, 1, 'item_standard', 1);
